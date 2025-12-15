61b8a67a33f17602c9eb069753423e51edff2e6f
aboudi-a11y <omarmoubarik07@gmail.com>
2025-12-15 03:11:51 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -59,6 +59,15 @@ const nextConfig = {
         ]
       },
       {
+  source: "/favicon.ico",
+  headers: [
+    {
+      key: "Cache-Control",
+      value: "public, max-age=31536000, immutable"
+    }
+  ]
+}
+      {
         source: "/images/:path*",
         headers: [
           {
