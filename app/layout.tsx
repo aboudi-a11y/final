ac3d8b0b4c1dd280049c16d45ba250fd986a1d56
aboudi-a11y <omarmoubarik07@gmail.com>
2025-12-19 20:43:36 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import Script from "next/script"
 import { SkipToContent } from "@/components/accessibility-skip-link"
+import FacebookPixel from './components/facebookpixel'
 
 const _geist = Geist({ subsets: ["latin"], display: "swap", preload: true })
 const _geistMono = Geist_Mono({ subsets: ["latin"], display: "swap" })
@@ -259,9 +260,9 @@ export default function RootLayout({
         <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       </head>
 
-      <body className="font-sans antialiased">
-        <SkipToContent />
-        <main id="main-content">{children}</main>
+     <body>
+        <FacebookPixel />  {/* initialisation Meta Pixel côté client */}
+        {children}
       </body>
     </html>
   )
