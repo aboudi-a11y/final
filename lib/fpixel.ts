04095104b561d23c39c7607ba0d2a9662bf722e2
aboudi-a11y <omarmoubarik07@gmail.com>
2025-12-19 21:04:49 +0100

    Update fpixel.ts

--- lib/fpixel.ts
+++ lib/fpixel.ts
@@ -1,8 +1,6 @@
-// lib/facebookPixel.js
-
+// lib/fpixel.ts
 export const FB_PIXEL_ID = '1419450969751777'
 
-// Initialisation du pixel
 export const initFacebookPixel = () => {
   if (!window.fbq) {
     !(function(f,b,e,v,n,t,s){
@@ -10,15 +8,14 @@ export const initFacebookPixel = () => {
       if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[]; t=b.createElement(e); t.async=!0;
       t.src=v; s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)
     })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-    
-    window.fbq('init', FB_PIXEL_ID);
-    window.fbq('track', 'PageView');
+
+    window.fbq('init', FB_PIXEL_ID)
+    window.fbq('track', 'PageView')
   }
 }
 
-// Fonction pour tracker un lead
-export const trackLead = () => {
+export const event = (name: string, params?: Record<string, any>) => {
   if (window.fbq) {
-    window.fbq('track', 'Lead');
+    window.fbq('track', name, params)
   }
 }
