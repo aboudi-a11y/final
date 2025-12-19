21f2ebcbbbd296d04a39b8b6c885b90b040efade
aboudi-a11y <omarmoubarik07@gmail.com>
2025-12-19 20:06:45 +0100

    Update fpixel.ts

--- lib/fpixel.ts
+++ lib/fpixel.ts
@@ -1,17 +1,24 @@
-declare global {
-  interface Window {
-    fbq: any;
+// lib/facebookPixel.js
+
+export const FB_PIXEL_ID = '1419450969751777'
+
+// Initialisation du pixel
+export const initFacebookPixel = () => {
+  if (!window.fbq) {
+    !(function(f,b,e,v,n,t,s){
+      if(f.fbq) return; n=f.fbq=function(){ n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments) };
+      if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[]; t=b.createElement(e); t.async=!0;
+      t.src=v; s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    
+    window.fbq('init', FB_PIXEL_ID);
+    window.fbq('track', 'PageView');
   }
 }
 
-export const pageview = () => {
-  if (typeof window !== "undefined" && window.fbq) {
-    window.fbq("track", "PageView");
+// Fonction pour tracker un lead
+export const trackLead = () => {
+  if (window.fbq) {
+    window.fbq('track', 'Lead');
   }
-};
-
-export const event = (name: string, options = {}) => {
-  if (typeof window !== "undefined" && window.fbq) {
-    window.fbq("track", name, options);
-  }
-};
+}
